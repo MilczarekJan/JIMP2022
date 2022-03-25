9caadaf922f9d30c5bd1693db6b9e321bd806dc7
stasiekq <staszek5286@gmail.com>
2022-03-25 18:20:13 +0100

    main input

--- main.c
+++ main.c
@@ -2,18 +2,77 @@
 #include <stdlib.h>
 #include <getopt.h>
 
+void input_errors_management(int opt, char *name, char *mod, int fromvert, int tovert, char *filename, double fromrange, double torange)
+{
+    if(torange > fromrange) //if fromrange and torange confused, swap them
+    {
+        double buf = torange;
+        torange = fromrange;
+        fromrange = buf;
+    }
+
+    if(filename == NULL) // If file not given, exit program
+    {
+        printf("File name not given! Ending program...");
+        exit(EXIT_FAILURE);
+    }
+
+    if(mod == NULL) // If mod not given, exit program
+    {
+        printf("Program mode (save/write) not given! Ending program...");
+        exit(EXIT_FAILURE);
+    }
+
+    if(fromvert == -1 || tovert == -1)
+    {
+        printf("Dijkstra vertexes not given! Ending program...");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char **argv)
 {
-    int opt;
+    int     opt;
+    char    *name = NULL;
+    char    *mod = NULL;
+    int     fromvert = -1;
+    int     tovert = -1;
+    char    *filename = NULL;
+    double  fromrange = 0;
+    double  torange = 1;
+    /*
+    m - mod(read/write)
+    f - z ktorego wierzcholka szukanie
+    t - do ktorego wierzcholka szukanie (sciezki)
+    */
 
-    while((opt = getopt(argc, argv, "a:b:c:")) != -1)
+    while((opt = getopt(argc, argv, "m:f:t:s:n:r:y:z:")) != -1)
     {
         switch (opt)
         {
-            case 'a':
-            break;
+            case 'm':
+                mod = optarg;
+                break;
+            case 'f':
+                fromvert = atoi(optarg);
+                break;
+            case 't':
+                tovert = atoi(optarg);
+                break;
+            case 'n':
+            case 'r':
+                filename = optarg;
+                break;
+            case 'y':
+                fromrange = atof(optarg);
+                break;
+            case 'z':
+                torange = atof(optarg);
+                break;
         }
     }
 
-    return 0;
+    input_errors_management(opt, name, mod, fromvert, tovert, filename, fromrange, torange);
+
+        return 0;
 }
